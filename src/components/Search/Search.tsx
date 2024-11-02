4d39000c2d91c606a1be048e2fb01edaa757b9a3
jaeheon0826 <jaeheon0826@jbnu.ac.kr>
2024-11-02 23:05:50 +0900

    feat: Search 컴포넌트 구현
    Search.tsx: 기본 틀 구현
    Search.css: 기본 틀에 관한 style 적용

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,1 +1,13 @@
-export {};
+import React from 'react';
+import './Search.css';
+
+const Search: React.FC = () => {
+    return (
+        <div className="search">
+            <h1>Search</h1>
+            <p>Find your favorite movies and shows.</p>
+        </div>
+    );
+};
+
+export default Search;
