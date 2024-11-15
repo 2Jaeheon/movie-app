18f2cc0f11588b285123883254584f766df3e08b
jaeheon0826 <jaeheon0826@jbnu.ac.kr>
2024-11-16 02:54:44 +0900

    feat: env로 관리

--- src/services/api/tmdbAPI.ts
+++ src/services/api/tmdbAPI.ts
@@ -1,1 +1,8 @@
-export {}
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+
+if (!API_KEY) {
+    throw new Error('Missing TMDB API Key!');
+}
+
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const API_PARAMS = `?api_key=${API_KEY}`;
