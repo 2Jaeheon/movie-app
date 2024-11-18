780bd1b21b04aba1b04dcb41ca86ad45fec62c25
jaeheon0826 <jaeheon0826@jbnu.ac.kr>
2024-11-18 12:28:27 +0900

    feat: Create Movie Card Component

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,1 +1,26 @@
-export {}
+import {Movie} from "../../models/Movie";
+import "./MovieCard.css";
+
+interface MovieCardProps {
+    movie: Movie;
+}
+
+const MovieCard: React.FC<MovieCardProps> = ({movie}) => {
+    return (
+        <div className="movie-card">
+            <img
+                className="movie-poster"
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+            />
+            <div className="movie-info">
+                <h3 className="movie-title">{movie.title}</h3>
+                <div className="movie-rating">
+                    ⭐ {movie.vote_average.toFixed(1)} / 10
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MovieCard;
