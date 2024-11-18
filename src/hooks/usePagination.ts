ddf2dbd46f93caf8081cf4c5b4ab1d48f2782862
jaeheon0826 <jaeheon0826@jbnu.ac.kr>
2024-11-18 19:07:29 +0900

    feat: develop react hook

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,1 +1,19 @@
-export {}
+import {useState} from "react";
+
+export const usePagination = () => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+
+    const goToNextPage = () => setCurrentPage((prev) => Math.min(prev + 1, totalPages));
+    const goToPrevPage = () => setCurrentPage((prev) => Math.max(prev - 1, 1));
+    const setPage = (page: number) => setCurrentPage(page);
+
+    return {
+        currentPage,
+        totalPages,
+        setTotalPages,
+        goToNextPage,
+        goToPrevPage,
+        setPage,
+    };
+};
