37aca9ef18b37cffa9d2fd7ed92ce4f5b9f40da4
jaeheon0826 <jaeheon0826@jbnu.ac.kr>
2024-11-19 12:37:07 +0900

    feat: Wishlist View 구현

--- src/views/WishlistView/WishlistView.tsx
+++ src/views/WishlistView/WishlistView.tsx
@@ -1,5 +1,33 @@
-const WishlistView = () => {
-    return <div>WishList View</div>;
+import React from "react";
+import {usePreference} from "../../hooks/usePreference";
+import MovieCard from "../../components/MovieCard/MovieCard";
+import "./WishlistView.css";
+
+const WishlistView: React.FC = () => {
+    const {wishlist, removeFromWishlist} = usePreference();
+
+    return (
+        <div className="wishlist-view">
+            <h1 className="wishlist-title">🎥 My Wishlist</h1>
+            {wishlist.length === 0 ? (
+                <p className="empty-message">찜한 영화가 없습니다. 마음에 드는 영화를 추가해 보세요!</p>
+            ) : (
+                <div className="wishlist-container">
+                    {wishlist.map((movie) => (
+                        <div key={movie.id} className="wishlist-card">
+                            <MovieCard movie={movie}/>
+                            <button
+                                className="wishlist-remove-button"
+                                onClick={() => removeFromWishlist(movie.id)}
+                            >
+                                ❌ 제거
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default WishlistView;
